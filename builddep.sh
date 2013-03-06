b9a948a2955070ae52cfabec047732e96146c93a
Ian-Woo Kim <ianwookim@gmail.com>
2013-03-06 02:54:06 -0500

    add travis-ci support

--- builddep.sh
+++ builddep.sh
@@ -1,10 +1,30 @@
 #!/bin/bash 
 
-ghc-pkg list
+cabal install transformers
+cabal install hscolour
+# libghc-hscolour-dev libghc-hstringtemplate-dev gtk2hs-buildtools libghc-gtk-dev libghc-gtk-doc 
+
 mkdir deps
 git clone https://github.com/wavewave/devadmin.git deps/devadmin
 cd deps/devadmin ; cabal install --force-reinstalls ; cd ../../
 $HOME/.cabal/bin/build cloneall --config=build.conf
+
+# for dep installation
 $HOME/.cabal/bin/build bootstrap --config=build.conf
+
+# for documentation of dep packages
+$HOME/.cabal/bin/build haddockboot --config=build.conf 
+
+# for documentation of this package
+cabal install  --enable-documentation
+cabal haddock --hyperlink-source
+cabal copy 
+
+tar cvzf LHCOAnalysis-type.tar.gz $HOME/.cabal/share/doc/LHCOAnalysis* 
+echo $CR | curl --digest -T LHCOAnalysis-type.tar.gz -K - $SRVRURL 
+
+
+# this is needed for checking
 cabal install --enable-tests
+
 
